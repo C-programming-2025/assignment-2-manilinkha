837ec5d6a534271f9ff513c92ef2eaf1f4521d96
manilinkha <manilinkha2061@gmail.com>
2025-09-06 13:43:37 +0545

    Assignment completed

--- src/q1.c
+++ src/q1.c
@@ -1,3 +1,9 @@
+/* Write a C program that takes an integer input from the user and determines if it is positive, negative, or zero.
+Expected Output:
+If n > 0, print Positive (case-insensitive, extra messages allowed).
+If n < 0, print Negative (case-insensitive, extra messages allowed).
+If n == 0, print Zero (case-insensitive, extra messages allowed).
+*/
 #include <stdio.h>
 int main(){
     int num;
