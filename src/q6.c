b17fbe3728f081e2b278c89912b5c9c07c05a736
manilinkha <manilinkha2061@gmail.com>
2025-09-06 14:32:18 +0545

    assignment completed

--- src/q6.c
+++ src/q6.c
@@ -5,9 +5,7 @@ int main()
     scanf("%d", &n);
     do
     {
-        printf("%d * %d = %d", n, i, n * i);
-        if (i < 10)  
-            printf("\n");
+        printf("%d * %d = %d\n", n, i, n * i);
         i++;
     } while (i <= 10);
     return 0;
